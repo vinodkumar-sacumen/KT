9d96c0638e7661f98597445555d08ca478b089b9
vinodkumar-sacumen <vinod.kumar@sacumen.com>
2023-04-27 11:01:53 +0530

    added docstring

--- src/demo.py
+++ src/demo.py
@@ -7,6 +7,15 @@ print(my_func("Python developer"))
 
 
 def add(num1: int, num2: int)-> int:
+    """Sum of 2 numbers.
+
+    Args:
+        num1 (int): a number.
+        num2 (int): another number.
+
+    Returns:
+        int: result of int which is sum of num1 and num2.
+    """
     return num1 + num2
     
 
