e609ac75b178fda9bafdbd56a90151841c391d7b
vinodkumar-sacumen <vinod.kumar@sacumen.com>
2023-06-12 12:31:50 +0530

    push for clearing commits

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -60,4 +60,6 @@ def test_fetch_mocks_failure(mocker: Any) -> None:
 def test_fetch_vcr()-> None:
     """Test case for fetch method using VCR"""
     response = fetch(url="https://api.publicapis.org/entries")
+    data = response.json()
     assert response.status_code == 200
+    assert data['count'] == 1425
