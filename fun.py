19bbe909a61a5c413c4b741a6dc1188dc7297952
ashwinikamble-sacumen <136899860+ashwinikamble-sacumen@users.noreply.github.com>
2023-06-18 19:57:56 +0530

    Create fun.py
    Added new functions

---
+++ fun.py
@@ -0,0 +1,28 @@
+# write a python function to create list and print that list where the values are square of numbers between 1 and 20 
+
+# def printList():
+#     list_ = list()       #create empty list to append values
+
+#     for val in range(1,21):
+#         list_.append(val**2)
+
+#     print(list_)
+
+#     #fucntion call 
+# printList()
+
+
+
+# write function decorators
+
+def to_capital(fun):
+    def inner():
+        a = fun()
+        return a.upper()
+    return inner
+
+
+# @to_capital
+def greet():
+    return "fork demo"
+print(greet())
