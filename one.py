6ad471bb8bb896b15bc196f296b7da3ab09b8cb7
ashwinikamble-sacumen <ashwini.kamble@sacumen.com>
2023-06-26 19:34:22 +0530

    files modified

---
+++ one.py
@@ -0,0 +1,35 @@
+import pytest
+import requests, pytest_mock, pytest_vcr
+from requests import Response
+from tests.mocks import functions
+
+
+# def fetch(url):
+#     response = requests.get(url)
+#     print(response.status_code)
+#     print(response.content)
+#     return response
+
+
+# print(fetch(url="https://api.publicapis.org/entries"))
+
+
+def fetch(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    count = data["count"]
+    count1 = 1425
+    if count == count1:
+        print(f"count = {count}")
+    else:
+        print(f"count={count1}")
+
+
+fetch("https://api.publicapis.org/entries")
+
+
+# @pytest.mark.vcr("tests/cassettes/")
+# def test_fetch_vcr(s):
+#     response = fetch(url="https://api.publicapis.org/entries")
+#     assert response.status_code == 200
---
+++ test_one.py
@@ -0,0 +1,40 @@
+import pytest, requests
+
+from requests import Response
+from one import fetch
+
+# from mocks.functions import mock_api_success, mock_api_failure
+
+
+def test_fetch():
+    response = fetch(url="https://api.publicapis.org/entries")
+    data = response.json()
+    count = data["count"]
+    assert response.status_code == 200
+    assert data["count"] == 1425
+
+
+# def test_fetch():
+#     response = fetch(url=)
+#     data = response.json()
+#     with pytest.raises(Exception):
+#         fetch()
+
+
+"""the process of adding dummy data called MOCKING"""
+
+
+# def test_fetch_mocks(mocker):
+#     mocker.patch("requests.get", mock_api_success)
+#     response = fetch(url="test_url")
+#     data = response.json()
+#     assert response.status_code == 200
+#     assert data["count"] == 1425
+
+
+# def test_fetch_mocks_failure(mocker):
+#     mocker.patch("requests.get", mock_api_failure)
+#     response = fetch(url="test_url")
+#     data = response.json()
+#     assert response.status_code == 403
+#     assert data["Error"] == "Invalid Authentication"
