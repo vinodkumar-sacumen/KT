a68417a3adcfd87512662e7fedf15d5f7f67525c
ashwinikamble-sacumen <ashwini.kamble@sacumen.com>
2023-07-06 14:53:24 +0530

    change in test case

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,16 +1,24 @@
 import pytest
 import logging
 import requests
+import pytest_mock
 from unittest import mock
 from src.fetch import fetch
 
-
-def test_fetch(caplog):
+    
+def test_fetch():
     url = "https://api.publicapis.org/entries"
-    caplog.set_level(logging.ERROR)
+    logging.basicConfig(level=logging.DEBUG)
+    log_messages = []
+    
+    def log_error(msg):
+        log_messages.append(msg)
+    logging.debug = log_error
     result = fetch(url)
     assert result is not None
-    assert len(caplog.records)==0
-    caplog.handler.stream.close()
+    
+    with open("fetched.log",'w') as log_file:
+        log_file.write("\n".join(log_messages))    
+ 
     
     
