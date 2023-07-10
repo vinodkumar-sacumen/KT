0a35fbe82c5f4e08f0eb4801c60c94123c5f71c4
ashwinikamble-sacumen <ashwini.kamble@sacumen.com>
2023-07-10 14:09:27 +0530

    modified file test_fetch.py

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,21 +1,34 @@
-import logging
+import pytest
 from src.fetch import fetch
+from tests.mocks.functions import mock_api_success, mock_api_failure
+
+
+def test_fetch():
+    
+    response = fetch(url="https://api.publicapis.org/entries")
+    with pytest.raises(AttributeError):
+        assert response.status_code == 200
+
+
+def test_fetch_mocks(mocker):
+    mocker.patch("requests.get", mock_api_success)
+    response = fetch(url="test_url")
+    with pytest.raises(AttributeError):
+        assert response.status_code == 200
+    
+   
+def test_fetch_mocks_failure(mocker):
+    mocker.patch("requests.get", mock_api_failure)
+    response = fetch(url="test_url")
+    with pytest.raises(AttributeError):
+        assert response.status_code == 403
 
     
-def test_fetch():
-    url = "https://api.publicapis.org/entries"
-    
-    logging.basicConfig(level=logging.DEBUG)
-    log_messages = []
-    
-    def log_error(msg):
-        log_messages.append(msg)
-    logging.debug = log_error
-    result = fetch(url)
-    assert result is not None
-    
-    with open("fetched.log",'w') as log_file:
-        log_file.write("\n".join(log_messages))    
- 
+@pytest.mark.vcr
+def test_fetch_with_vcr():
+    response = fetch(url="https://api.publicapis.org/entries")
+    with pytest.raises(AttributeError):
+        assert response.status_code == 200
+
 
     
