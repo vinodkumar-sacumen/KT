d86deda3036d9e25f5d8caf965d7b2cd26716bac
ashwinikamble-sacumen <ashwini.kamble@sacumen.com>
2023-07-10 14:08:56 +0530

    modified file fetch.py

--- src/fetch.py
+++ src/fetch.py
@@ -1,28 +1,34 @@
 import requests
-import logging
 
 
-def fetch(url):
+def fetch(url): 
     try:
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
-        
-        
-    except requests.exceptions.RequestException as e:       
-        """log error message"""
-        
-        logging.error(f"failed request : {str(e)}")
+    except requests.exceptions.RequestException as e:
         raise RuntimeError(str(e))
 
 
-"""logging configuration"""
-
-logging.basicConfig(
-    filename="fetched.log",
-    filemode="w",
-    level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
-
 fetch("https://api.publicapis.org/entries")
+
+
+def fetch_count_one(url):
+    
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            count = data["count"]
+            count1 = 1425
+            if count == count1:
+                print(f"count = {count}")
+            else:
+                print(f"count={count1}")
+        else:
+            raise RuntimeError(f"requests failed{response.status_code}")
+    except requests.exceptions.RequestException as e1:
+        raise RuntimeError(f"An error occurred : {str(e1)}")
+
+
+fetch_count_one("https://api.publicapis.org/entries")
