7c5b16ee5a5b03032010a0ad43ec02def83fe335
ashwinikamble-sacumen <ashwini.kamble@sacumen.com>
2023-07-12 12:49:29 +0530

    commit done

--- src/fetch.py
+++ src/fetch.py
@@ -1,7 +1,8 @@
-import requests
-from requests import Response
+"""Fetch python file."""
+
 import logging
 
+import requests
 
 logging.basicConfig(
     filename="fetched.log",
@@ -11,11 +12,19 @@ logging.basicConfig(
 )
 
 
-def fetch(url):
-    response = requests.get(url)
+def fetch(url: str) -> str:
+    """Passing Url.
+
+    Args:
+        url (str): a string.
+
+    Returns:
+        str:Fetching data from given API.
+    """
+    response = requests.get(url, timeout=10)
     if response.status_code == 200:
         data = response.json()
-        logging.info(f"Api call {url} successful")
+        logging.info("Api call successful for %s", url)
         return data["count"]
     return "wrong url"
 
@@ -24,12 +33,22 @@ fetch("https://api.publicapis.org/entries")
 
 
 def add(num1, num2):
+    """Sum of two number
+
+    Args:
+        num1 (int): a number.
+        num2 (int): another number.
+
+    Returns:
+        int:result of int which is sum of num1 and num2.
+    """
     try:
         result = num1 + num2
-        logging.info(f"Addition success {result}")
+        logging.info("Addition success %s ", result)
         return result
     except TypeError:
         logging.error("Invalid Input")
+    return None
 
 
 add(10, 10)
@@ -37,12 +56,22 @@ add(10, "hi")
 
 
 def divide(num1, num2):
+    """Division of two number.
+
+    Args:
+        num1 (int): a number.
+        num2 (int): another number.
+
+    Returns:
+        int: result of int which is sum of num1 and num2.
+    """
     try:
         result = num1 / num2
-        logging.info(f"Division success {result}")
-        return result
+        logging.info("Division success %s ", result)
+        return f"result is {result}"
     except ZeroDivisionError:
         logging.error("Invalid Input")
+    return None
 
 
 divide(10, 2)
--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,26 +1,32 @@
-import pytest
-from src.fetch import fetch, add, divide
+""" Test cases python file."""
 
-from tests.mocks.functions import mock_api_success, mock_api_failure
+import pytest
+
+from src.fetch import add, divide, fetch
+from tests.mocks.functions import mock_api_failure, mock_api_success
 
 
 def test_fetch():
+    """Test case for fetch method."""
     response = fetch(url="https://api.publicapis.org/entries")
     assert response == 1425
 
 
 def test_fetch_failure():
+    """Test case for failure."""
     response = fetch(url="https://api.publicapis.org/entriesj")
     assert response == "wrong url"
 
 
 def test_fetch_mocks(mocker):
+    """Test case for mocks."""
     mocker.patch("requests.get", mock_api_success)
     response = fetch(url="test_url")
     assert response == 1425
 
 
 def test_fetch_mocks_failure(mocker):
+    """Test case for mock failure."""
     mocker.patch("requests.get", mock_api_failure)
     response = fetch(url="test_url")
     assert response == "wrong url"
@@ -28,35 +34,42 @@ def test_fetch_mocks_failure(mocker):
 
 @pytest.mark.vcr
 def test_fetch_with_vcr():
+    """Test case for pytest_vcr."""
     response = fetch(url="https://api.publicapis.org/entries")
     assert response == 1425
 
 
 def test_add_success():
+    """Test case for add method."""
     result = add(num1=10, num2=20)
     assert result == 30
 
 
 def test_add_no_params():
+    """Test case for no params."""
     with pytest.raises(TypeError):
         add()
 
 
 def test_add_failure():
+    """Test case for addition failure."""
     result = add(num1=5, num2="Hi")
     assert result is None
 
 
 def test_divide_success():
+    """Test case for division method"""
     result = divide(num1=10, num2=5)
     assert result == 2.0
 
 
 def test_divide_no_params():
+    """Test case for no params division."""
     with pytest.raises(TypeError):
         divide()
 
 
 def test_divide_failure():
+    """Test case for division failure"""
     result = divide(num1=5, num2=0)
     assert result is None
