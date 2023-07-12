2c126b3044268999d82e3a071d81c48ee6f4985e
chintan2211 <cjgengineer@gmail.com>
2023-07-12 14:12:21 +0530

    completed task1

--- src/gorest.py
+++ src/gorest.py
@@ -1,1 +1,20 @@
-"""Python file that fetches user and user details api."
+
+"""Python file that fetches user and user details api."""
+
+import requests
+import json
+
+api_url = "https://gorest.co.in/public/v2/users"
+user_details_api_url = "https://gorest.co.in/public/v2/users/"
+
+def fetch_user_details():
+    user_details_list = []
+    user_list = (requests.get(url=api_url)).json()
+    for user in user_list:
+        user_id = user["id"]
+        user_details = (requests.get(url = user_details_api_url+str(user_id))).json()
+        user_details_list.append(user_details)
+    return user_details_list
+
+response = fetch_user_details()
+print(response)
