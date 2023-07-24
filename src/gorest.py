ddda59a21e77514fc43a400270e5aa0edc457a49
ayushi gadhiya <ayushi.gadhiya@sacumen.com>
2023-07-24 10:03:29 +0530

    written test cases for failure scenarios

--- src/gorest.py
+++ src/gorest.py
@@ -9,14 +9,19 @@ user_details_api_url = "https://gorest.co.in/public/v2/users/"
 
 def fetch(api_url,user_details_api_url):
     user_id_list = user_list(api_url)
-    user_details_list = user_details(user_details_api_url,user_id_list)
-    return user_details_list
+    if user_id_list:
+        user_details_list = user_details(user_details_api_url,user_id_list)
+        return user_details_list
+    else:
+        return user_id_list
 
 def user_list(url):
     user_id_list = []
     user_list = requests.get(url=url)
     if user_list.status_code == 200:
         user_list = user_list.json()
+    else:
+        return user_id_list
     for user in user_list:
         user_id_list.append(user["id"])
     return user_id_list
@@ -27,6 +32,8 @@ def user_details(url,user_id_list):
         response = requests.get(url = url+str(user_id))
         if response.status_code == 200:
             user_details_list.append(response.json())
+        else:
+            user_details_list
     return user_details_list
 
 response = fetch(api_url,user_details_api_url)
--- tests/mocks/function_gorest.py
+++ tests/mocks/function_gorest.py
@@ -5,7 +5,7 @@ from requests import Response
 
 def mock_user_details_success(*args: List[Any], **kwargs: Dict[str, Any]):
     """Mock for response success."""
-    result = {'id': 3786877, 'name': 'Sen. Aishani Nambeesan', 'email': 'aishani_sen_nambeesan@sporer-gottlieb.test', 'gender': 'female', 'status': 'active'}     
+    result = {'id': 12345, 'name': 'Aishwarya', 'email': 'aisshwarya@test.com', 'gender': 'female', 'status': 'active'}     
     
     response = Response()
     response.status_code = 200
@@ -15,10 +15,32 @@ def mock_user_details_success(*args: List[Any], **kwargs: Dict[str, Any]):
 
 def mock_user_list_success(*args: List[Any], **kwargs: Dict[str, Any]):
     """Mock for response success."""
-    result = [{'id': 3786877, 'name': 'Sen. Aishani Nambeesan', 'email': 'aishani_sen_nambeesan@sporer-gottlieb.test', 'gender': 'female', 'status': 'active'}]
+    result = [{'id': 12345, 'name': 'Aishwarya', 'email': 'aisshwarya@test.com', 'gender': 'female', 'status': 'active'}]
     
     response = Response()
     response.status_code = 200
     response._content = bytes(json.dumps(result),encoding="utf-8")
     
+    return response
+
+def mock_user_details_failure(*args: List[Any], **kwargs: Dict[str, Any]) -> Response:
+    """Mock for authentication error."""
+    result = {
+        "Error": "Invalid Authentication"
+    }
+
+    response = Response()
+    response.status_code = 403
+    response._content = bytes(json.dumps(result), encoding="utf-8")
+    return response
+
+def mock_user_list_failure(*args: List[Any], **kwargs: Dict[str, Any]) -> Response:
+    """Mock for authentication error."""
+    result = [{
+        "Error": "Invalid Authentication"
+    }]
+
+    response = Response()
+    response.status_code = 403
+    response._content = bytes(json.dumps(result), encoding="utf-8")
     return response
