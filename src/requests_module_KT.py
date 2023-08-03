b6787674568f3a3db0307572dde494ea7a850240
Sharon Renisha <sharonr.dsouza@sacumen.com>
2023-08-03 10:06:33 +0530

    Renamed the method to fetch

--- src/requests_module_KT.py
+++ src/requests_module_KT.py
@@ -2,12 +2,9 @@
 import requests
 
 def fetch():
-    '''
-    Makes an api call and returns the count 
-    '''
     response = requests.get(url = 'https://api.nationalize.io/?name=nathaniel')
     result = response.json().get('count')
     return result
 
-count = return_count()
+count = fetch()
 print(count)
