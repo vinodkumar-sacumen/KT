925324633872525e97b9dedf4846de8b7ba114bf
vinodkumar-sacumen <vinod.kumar@sacumen.com>
2023-08-08 13:31:30 +0530

    added sub method

--- src/requests_module_KT.py
+++ src/requests_module_KT.py
@@ -8,3 +8,15 @@ def fetch():
 
 count = fetch()
 print(count)
+
+
+def subtract(num1: int, num2: int) -> int:
+    """Substraction of 2 numbers.
+    Args:
+        num1 (int): First integer.
+        num2 (int): Second integer.
+
+    Returns:
+        int: subtracted value.
+    """
+    return num1-num2 if num1 > num2 else num2-num1
