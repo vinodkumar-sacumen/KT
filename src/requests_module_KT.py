8d4f210b4c8e302a41ffb9dbe2d543ee6b6455eb
Sharon Renisha <sharonr.dsouza@sacumen.com>
2023-08-08 12:28:23 +0530

    Updated requests_module_KT.py with a new method to add two numbers

--- src/requests_module_KT.py
+++ src/requests_module_KT.py
@@ -8,3 +8,14 @@ def fetch():
 
 count = fetch()
 print(count)
+
+def add_two_numbers(num1,num2):
+    '''
+    Adds two numbers
+     
+    '''
+    result = num1 + num2 
+    return result
+
+sum_value = add_two_numbers(20,20)
+print(sum_value)
