b3888a40f82702143bc613e305b59b98e65b5d16
Ganesh-sacumen <ganesh.p@sacumen.com>
2024-03-12 15:44:11 +0530

    added docstring and type hinting parameter

--- src/ganesh.py
+++ src/ganesh.py
@@ -1,4 +1,18 @@
-def calculations(num1,num2):
+""""This function will add two number
+    which we have specified
+"""
+
+def calculations(num1:int,num2:int):
+    """_summary_
+
+    Args:
+        num1  integer : First Arguement is integer
+        num2  integer : Second Argument is integer
+
+    Returns:
+        int: addition of two number
+    """
+    
     return num1 + num2
 
-calculations(10,20)
+print(calculations(10,20))
