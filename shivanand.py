1e23ba187e0bb38f46e085a994cbcc6ffff30968
shivanand-sacumen <shivanand.patil@sacumen.com>
2024-04-15 18:07:55 +0530

    added task2

--- shivanand.py
+++ shivanand.py
@@ -1,4 +1,4 @@
-""" a simple api call """
+""" set of tasks """
 import requests
 
 BASE_URL = "https://reqres.in/api/users"
@@ -19,5 +19,17 @@ def sub(num1: int, num2: int) -> int:
     sub = num1 - num2
     return sub
 
+# task 2 
+
+class TASK2:
+    def add_no(self, num1:int, num2:int) ->int:
+        """Addition of 2 numbers.
+    Args:
+        num1 (int): first number.
+        num2 (int): second number.
+    Returns:
+        int: returns sum of num1 and num2
+    """
+        return num1+num2
 
     
