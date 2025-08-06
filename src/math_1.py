f2260c724f8063e00c720e1aedaa9024ddc84ed9
Haris-sacumen <Haris.pc@sacumen.com>
2025-08-06 17:38:43 +0530

    fixed correct indentation and doc string

--- src/math_1.py
+++ src/math_1.py
@@ -1,21 +1,21 @@
 class Math:
    def __init__(self, a: float, b: float) -> None:
-      self.a: float = a
-      self.b: float = b
+       self.a: float = a
+       self.b: float = b
 
    def add(self) -> float:
-      return self.a + self.b
+       return self.a + self.b
 
    def subtract(self) -> float:
-      return self.a - self.b
+       return self.a - self.b
 
    def multiply(self) -> float:
-      return self.a * self.b
+       return self.a * self.b
 
    def divide(self) -> float | str:
-      if self.b == 0:
-         return "Cannot divide by zero"
-      return self.a / self.b
+       if self.b == 0:
+          return "Cannot divide by zero"
+       return self.a / self.b
 
 
 if __name__ == "__main__":
