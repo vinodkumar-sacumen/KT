13b8b72799cb609777dab38bd60531dcd75abfde
rohan-sacumen <rohan.yalamali@sacumen.com>
2025-08-06 23:07:12 +0530

    Running pre-commit checks

--- src/rohan.py
+++ src/rohan.py
@@ -1,5 +1,11 @@
-"""
-    A class to represent a rectangle and perform basic geometric calculation.
+"""A class to represent a rectangle and perform basic geometric calculation."""
+
+
+class Rectangle:
+    """Represents a rectangle.
+
+    This class stores the width and height of a rectangle and provides
+    a method to calculate its area.
 
     Attributes:
         width (float): The width of the rectangle.
@@ -7,30 +13,36 @@
 
     Methods:
         area(): Returns the area of the rectangle.
-"""
-class Rectangle:
-    
+    """
+
     def __init__(self, width: float, height: float) -> None:
         """
-        Initializes a Rectangle instance with width and height.
+        Initialize a Rectangle instance with width and height.
 
         Args:
             width (float): The width of the rectangle.
             height (float): The height of the rectangle.
-
-	Returns:
-		None: It doesn't return anything.
         """
         self.width = width
         self.height = height
 
     def area(self) -> float:
         """
-        Calculates the area of the rectangle.
+        Calculate the area of the rectangle.
 
         Returns:
             float: The area of the rectangle.
         """
         return self.width * self.height
 
-obj = Rectangle(10.0, 20.0)
+    def perimeter(self) -> float:
+        """
+        Calculate the perimeter of the rectangle.
+
+        Returns:
+            float: The perimeter of the rectangle.
+        """
+        return 2 * (self.width + self.height)
+
+
+obj = Rectangle(30.0, 50.0)
