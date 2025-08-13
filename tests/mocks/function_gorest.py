9a3e8c837bb424ff728908d8dd3f82012ef55d35
vinodkumar-sacumen <vinod.kumar@sacumen.com>
2025-08-13 17:09:14 +0530

    added updates

--- tests/mocks/function_gorest.py
+++ tests/mocks/function_gorest.py
@@ -5,22 +5,22 @@ from requests import Response
 
 def mock_user_details_success(*args: List[Any], **kwargs: Dict[str, Any]):
     """Mock for response success."""
-    result = {'id': 12345, 'name': 'Aishwarya', 'email': 'aisshwarya@test.com', 'gender': 'female', 'status': 'active'}     
-    
+    result = {'id': 12345, 'name': 'Aishwarya', 'email': 'aisshwarya@test.com', 'gender': 'female', 'status': 'active'}
+
     response = Response()
     response.status_code = 200
     response._content = bytes(json.dumps(result),encoding="utf-8")
-    
+
     return response
 
 def mock_user_list_success(*args: List[Any], **kwargs: Dict[str, Any]):
     """Mock for response success."""
     result = [{'id': 12345, 'name': 'Aishwarya', 'email': 'aisshwarya@test.com', 'gender': 'female', 'status': 'active'}]
-    
+
     response = Response()
     response.status_code = 200
     response._content = bytes(json.dumps(result),encoding="utf-8")
-    
+
     return response
 
 def mock_user_details_failure(*args: List[Any], **kwargs: Dict[str, Any]) -> Response:
